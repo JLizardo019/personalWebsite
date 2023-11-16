1fee61365f8bbaa148a4840ef443437226d6b498
Julie Lizardo <44945863+JLizardo019@users.noreply.github.com>
2023-11-16 12:24:34 -0500

    fixing project image and small project page bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "750339a63920d754"; }
+/******/ 		__webpack_require__.h = function() { return "df62b3816d68ee01"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/mediaObject.js
+++ components/mediaObject.js
@@ -15,7 +15,7 @@ export default function MediaObject({ title, alt, tools, url, image, code, descr
             <h4 className="text-xl text-gray-900 font-bold">{title}</h4>
             <div className="flex">
               {code == "" ? <></>: <Link className="mr-3 flex items-center hover:text-[var(--c2)]" href={code} target="_blank">
-              <svg className="w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+              <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
 </svg>
 
